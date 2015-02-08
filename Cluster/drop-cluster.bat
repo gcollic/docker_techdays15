af041b94fc79464f4dbce6c0c9dff7601f33b826
Jean-Philippe Gouigoux <jp.gouigoux@free.fr>
2015-02-08 22:27:06 +0100

    Fichiers de définition du cluster CoreOS

---
+++ Cluster/drop-cluster.bat
@@ -0,0 +1,4 @@
+call azure account import C:\Users\jpgou_000\AbonnementAzure.publishsettings
+call azure vm delete -b -q coreos1
+call azure vm delete -b -q coreos2
+call azure vm delete -b -q coreos3
